65caeaba4893afeab768904fbb912ea595c62c07
FIAP <FIAP>
2019-08-31 12:18:08 -0300

    ok

--- NoPonto WatchKit Extension/InterfaceController.swift
+++ NoPonto WatchKit Extension/InterfaceController.swift
@@ -11,11 +11,31 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
+    
+    @IBOutlet weak var timer: WKInterfaceTimer!
+    @IBOutlet weak var textGroup: WKInterfaceGroup!
+    @IBOutlet weak var imageGroup: WKInterfaceGroup!
+    @IBOutlet weak var weightLabel: WKInterfaceLabel!
+    @IBOutlet weak var cookLabel: WKInterfaceLabel!
+    @IBOutlet weak var weightPicker: WKInterfacePicker!
+    @IBOutlet weak var temperaturePicker: WKInterfacePicker!
+    
+    @IBOutlet weak var temperatureLabel: WKInterfaceLabel!
+    
+    @IBOutlet weak var slider: WKInterfaceSlider!
+    @IBOutlet weak var timerButton: WKInterfaceButton!
+    
+    
+    var kg: Double = 0.1
+    var cookTemp = MeatTemperature.rare
+    var timerRunning = false
+    var increment = 0.1
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
+        imageGroup.setHidden(true)
+        setupPickers()
+        updateConfiguration()
     }
     
     override func willActivate() {
@@ -27,5 +47,99 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
+    
+    @IBAction func onMinusButton() {
+        if kg > 0.1 {
+            kg -= increment
+            updateConfiguration()
+        }
+    }
+    
+    @IBAction func onPlusButton() {
+        if kg < 1.0 {
+            kg += increment
+            updateConfiguration()
+        }
+    }
+    
+    @IBAction func onTempChange(_ value: Float) {
+        if let temp = MeatTemperature(rawValue: Int(value)) {
+            cookTemp = temp
+            updateConfiguration()
+            //Atualizando o outro painel
+            temperaturePicker.setSelectedItemIndex(Int(value))
+        }
+    }
+    
+    @IBAction func onTimerButton() {
+        if timerRunning {
+            timer.stop()
+            timerButton.setTitle("Start Timer")
+        } else {
+            let time = cookTemp.cookTimeForKg(kg)
+            timer.setDate(Date(timeIntervalSinceNow: time))
+            timer.start()
+            timerButton.setTitle("Stop Timer")
+        }
+        timerRunning = !timerRunning
+    }
+    
+    
+    @IBAction func onWeightChange(_ value: Int) {
+        kg = Double(value+1)*increment
+        updateConfiguration()
+    }
+    
+   
+    @IBAction func onTemperatureChange(_ value: Int) {
+        let temp = MeatTemperature(rawValue: value)!
+        cookTemp = temp
+        updateConfiguration()
+        temperatureLabel.setText(temp.stringValue)
+        //Atualizando o outro painel
+        slider.setValue(Float(value))
+    }
+    @IBAction func onModeChange(_ value: Bool) {
+        imageGroup.setHidden(!value)
+        textGroup.setHidden(value)
+        updateConfiguration()
+    }
+    
+    
+    //MARK: - Methods
+    
+    func setupPickers() {
+        //Picker de quantidade
+        var weightItems: [WKPickerItem] = []
+        for i in stride(from: 0.1, through: 1.0, by: increment) {
+            let item = WKPickerItem()
+            item.title = "\(get2DecimalPlaces(for: i))"
+            weightItems.append(item)
+        }
+        weightPicker.setItems(weightItems)
+        weightPicker.setSelectedItemIndex(0)
+        
+        //Picker de temperatura
+        var tempItems: [WKPickerItem] = []
+        for i in 1...4 {
+            let item = WKPickerItem()
+            item.contentImage = WKImage(imageName: "temp-\(i)")
+            tempItems.append(item)
+        }
+        temperaturePicker.setItems(tempItems)
+    }
+    
+    func get2DecimalPlaces(for number: Double) -> Double {
+        return Double(round(100*number)/100)
+    }
+    
+    func updateConfiguration() {
+        cookLabel.setText(cookTemp.stringValue)
+        kg = get2DecimalPlaces(for: kg)
+        weightLabel.setText("Total: \(kg) kg")
+    }
+    
 }
+
+
--- NoPonto WatchKit Extension/MeatTemperature.swift
+++ NoPonto WatchKit Extension/MeatTemperature.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+enum MeatTemperature: Int {
+    case rare = 0, mediumRare, medium, wellDone
+    
+    var stringValue: String {
+        let temperatures = ["Cru", "Mal passado", "Ao ponto", "Bem passado"]
+        return temperatures[self.rawValue]
+    }
+    
+    var timeModifier: Double {
+        let modifiers = [0.5, 0.75, 1.0, 1.5]
+        return modifiers[self.rawValue]
+    }
+    
+    func cookTimeForKg(_ kg: Double) -> TimeInterval {
+        let baseTime: TimeInterval = 1.3 * 60
+        let baseWeight = 0.5
+        let weightModifier: Double = kg/baseWeight
+        let tempModifier = self.timeModifier
+        return baseTime * weightModifier * tempModifier
+    }
+}
