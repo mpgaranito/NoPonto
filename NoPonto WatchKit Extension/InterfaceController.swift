4c875e630d4bbadeac7580fdcad9053f0b59e59b
FIAP <FIAP>
2019-08-31 09:43:07 -0300

    Initial Commit

---
+++ NoPonto WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  NoPonto WatchKit Extension
+//
+//  Created by Usuário Convidado on 31/08/19.
+//  Copyright © 2019 GARANITO ME. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ NoPonto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoPonto
+//
+//  Created by Usuário Convidado on 31/08/19.
+//  Copyright © 2019 GARANITO ME. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
