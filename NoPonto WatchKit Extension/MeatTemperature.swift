599e0276f2ff25944eb26224bcad87fa9f6c0b1e
FIAP <FIAP>
2019-08-31 12:00:26 -0300

    inicial

---
+++ NoPonto WatchKit Extension/MeatTemperature.swift
@@ -0,0 +1,9 @@
+//
+//  MeatTemperature.swift
+//  NoPonto WatchKit Extension
+//
+//  Created by Usuário Convidado on 31/08/19.
+//  Copyright © 2019 GARANITO ME. All rights reserved.
+//
+
+import Foundation
